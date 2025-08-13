1464734ccb97b91e027aa1ada513023c68fea5fb
futurepear <futurethepear@gmail.com>
2025-08-12 22:45:53 -0700

    init

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Vector {
+    int x;
+    int y;
+};
+
+void something() {
+    std::cout << "Hello, World!" << std::endl;
+}
+
+int main() {
+    something();
+    return 0;
+}
